d6aa8bb798020f82c30d59bb7b3571f1683d463c
caellex <157691477+caellex@users.noreply.github.com>
2024-01-31 14:10:32 +0100

    trying again
    try again

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-function pickUp(elem){
-if(elem.innerText == "") 
-return
-
-
-
-}
+function pickUpItem1() {
+    document.getElementById('gItem1').innerHTML = /*HTML*/ ``
+    
+    document.getElementById('iItem1').innerHTML = /*HTML*/ `<img src="assets/bagofholding.gif" />`
+    
+    
+    
+    }
+    function pickUpItem1() {
+    document.getElementById('itemMid')
+    }
