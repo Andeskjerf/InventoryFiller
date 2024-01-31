9fc2c78dcf057cbba59da737f56e2d6e9bb51174
caellex <157691477+caellex@users.noreply.github.com>
2024-01-31 14:40:00 +0100

    assets added, js functionality.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 function pickUpItem1() {
-    document.getElementById('gItem1').innerHTML = /*HTML*/ ``
-    
-    document.getElementById('iItem1').innerHTML = /*HTML*/ `<img src="assets/bagofholding.gif" />`
-    
-    
-    
-    }
-    function pickUpItem1() {
-    document.getElementById('itemMid')
-    }
+    document.getElementById('gItem1').innerHTML = /*HTML*/ 
+    ``
+    document.getElementById('iItem1').innerHTML = /*HTML*/
+    `<img src="assets/bagofholding.gif"/>`
+}
+function pickUpItem2() {
+    document.getElementById('gItem2').innerHTML = /*HTML*/ 
+    ``
+    document.getElementById('iItem2').innerHTML = /*HTML*/
+    `<img src="assets/bagofholding.gif"/>`
+}
+function pickUpItem3() {
+    document.getElementById('gItem3').innerHTML = /*HTML*/ 
+    ``
+    document.getElementById('iItem3').innerHTML = /*HTML*/
+    `<img src="assets/bagofholding.gif"/>`
+}
+function pickUpItem4() {
+    document.getElementById('gItem5').innerHTML = /*HTML*/ 
+    ``
+    document.getElementById('iItem4').innerHTML = /*HTML*/
+    `<img src="assets/bagofholding.gif"/>`
+}
+function pickUpItem5() {
+    document.getElementById('gItem7').innerHTML = /*HTML*/ 
+    ``
+    document.getElementById('iItem5').innerHTML = /*HTML*/
+    `<img src="assets/bagofholding.gif"/>`
+}
+function pickUpItem6() {
+    document.getElementById('gItem9').innerHTML = /*HTML*/ 
+    ``
+    document.getElementById('iItem6').innerHTML = /*HTML*/
+    `<img src="assets/bagofholding.gif"/>`
+}
