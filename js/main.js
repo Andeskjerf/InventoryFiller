5b0d12db2fb4e2059eb2b1a23a479757b8fe331b
Martin R <martin.ristvedt@gmail.com>
2024-02-01 14:07:35 +0100

    revehale

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
-function pickUpItem1() {
+let tittelitei = `
+    
+`
+
+function pickUpItem1(bagofholding) {
     document.getElementById('gItem1').innerHTML = /*HTML*/ 
     ``
+    tittelitei += bagofholding + ", "
     document.getElementById('iItem1').innerHTML = /*HTML*/
     `<img src="assets/bagofholding.gif"/>`
+    console.log(tittelitei)
 }
-function pickUpItem2() {
+function pickUpItem2(boots) {
+    tittelitei += boots + ", "
     document.getElementById('gItem2').innerHTML = /*HTML*/ 
     ``
     document.getElementById('iItem2').innerHTML = /*HTML*/
-    `<img src="assets/bagofholding.gif"/>`
+    `<img src="assets/boots.png"/>`
 }
-function pickUpItem3() {
+function pickUpItem3(quill) {
+    tittelitei += quill + ", "
     document.getElementById('gItem3').innerHTML = /*HTML*/ 
     ``
     document.getElementById('iItem3').innerHTML = /*HTML*/
-    `<img src="assets/bagofholding.gif"/>`
+    `<img src="assets/quill.gif"/>`
 }
-function pickUpItem4() {
+function pickUpItem4(bow) {
+    tittelitei += bow + ", "
     document.getElementById('gItem5').innerHTML = /*HTML*/ 
     ``
     document.getElementById('iItem4').innerHTML = /*HTML*/
-    `<img src="assets/bagofholding.gif"/>`
+    `<img src="assets/bow.gif"/>`
 }
-function pickUpItem5() {
+function pickUpItem5(armor) {
+    tittelitei += armor + ", "
     document.getElementById('gItem7').innerHTML = /*HTML*/ 
     ``
     document.getElementById('iItem5').innerHTML = /*HTML*/
-    `<img src="assets/bagofholding.gif"/>`
+    `<img src="assets/armor.png"/>`
 }
-function pickUpItem6() {
+function pickUpItem6(shield) {
+    tittelitei += shield + ", "
     document.getElementById('gItem9').innerHTML = /*HTML*/ 
     ``
     document.getElementById('iItem6').innerHTML = /*HTML*/
-    `<img src="assets/bagofholding.gif"/>`
+    `<img src="assets/shield.png"/>`
 }
